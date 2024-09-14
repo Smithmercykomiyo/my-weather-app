68aa5cd6d23add7d62debd88f20cd1b196708bdf
Smithmercykomiyo <smithmercykomiyo@gmail.com>
2024-09-15 00:28:04 +0100

    injected more weather data

--- SRC/index.js
+++ SRC/index.js
@@ -2,9 +2,39 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.response.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday"
+    "Monday"
+    "Tuesday"
+    "Wednesday"
+    "Thursday"
+    "Friday"
+    "Saturday"
+  ];
+  
+    let day = days[date.getDay()];
+
+if (minutes < 10) {
+    minutes = `0${minutes}`
+}
+return `${day} ${minutes}`
 }
 
 function searchCity(city) {
@@ -23,3 +53,5 @@ function handleSearchSubmit(event) {
 
 let formClassElement = document.querySelector("#form-class");
 formClassElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("paris")
