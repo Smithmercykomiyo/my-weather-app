50166ad82acbdf623e96bc2ea74974185b643c41
Smithmercykomiyo <smithmercykomiyo@gmail.com>
2024-09-14 22:49:04 +0100

    Added the search formalities using the api

--- SRC/index.js
+++ SRC/index.js
@@ -1,3 +1,18 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.response.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = math.round(temperature);
+}
+
+function searchCity(city) {
+  let apikey = "9eb43teo3c7bfa6d3d81e0dfa5bf1130";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-class-input");
