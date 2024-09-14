ebe75ac6a893c9c98dc93a0f3b79894d948c4536
Smithmercykomiyo <smithmercykomiyo@gmail.com>
2024-09-14 22:11:31 +0100

    Added basic search engine and updated the HTML

--- SRC/index.js
+++ SRC/index.js
@@ -1,2 +1,10 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#form-class-input");
+  console.log(searchInput.value);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
 let formClassElement = document.querySelector("#form-class");
-console.log(formClassElement);
+formClassElement.addEventListener("submit", handleSearchSubmit);
